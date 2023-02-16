4f663eb28c9a407cbc7a71db1a0be25b5666bc3f
JuanFrancisco21 <juanfranciscoaguilarsanchez@gmail.com>
2023-02-16 16:24:06 +0100

    Now work login but needs to pass access control

--- src/utility/Utils.js
+++ src/utility/Utils.js
@@ -62,8 +62,8 @@ export const getUserData = () => JSON.parse(localStorage.getItem('userData'))
  * @param {String} userRole Role of user
  */
 export const getHomeRouteForLoggedInUser = userRole => {
-  if (userRole === 'admin') return DefaultRoute
-  if (userRole === 'client') return '/access-control'
+  if (userRole === '0' ) return DefaultRoute
+  if (userRole === '1') return '/access-control'
   return '/login'
 }
 
