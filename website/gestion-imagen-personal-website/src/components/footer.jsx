c0a2abb63a9aa599c23c9f2fe46489d26fa821c2
Jose_Angel_Marin_Moreno <joseangelmarin1234@gmail.com>
2023-02-21 12:58:33 +0100

    footer 2.0

--- website/gestion-imagen-personal-website/src/components/footer.jsx
+++ website/gestion-imagen-personal-website/src/components/footer.jsx
@@ -10,8 +10,8 @@ function footer() {
 
   return (
     <div>
-      <footer class="site-footer">
-        <div class="container">
+      <footer class="footer">
+        {/*  <div class="container">
           <div class="row">
             <div class="col-2">
               <h6>Contactanos</h6>
@@ -30,13 +30,8 @@ function footer() {
             <div class="col-7">
               <section class="">
                 <form action="">
-
                   <div class="row d-flex justify-content-center">
-
-
                     <h6>¿Tienes alguna pregunta?</h6>
-
-
                     <div class="col-md-5 col-12">
 
                       <div class="form-outline form-white mb-2 bg-white">
@@ -48,27 +43,81 @@ function footer() {
                         <label class="form-label text-dark" for="form5Example21">Tu pregunta</label>
                       </div>
                     </div>
-
                     <div class="col-auto">
-
                       <button type="submit" id="Send" class="btn btn-outline-light">
                         Enviar
                       </button>
                     </div>
-
                   </div>
-
                 </form>
               </section>
-
-
             </div>
           </div>
+        </div> */}
 
+        <div class="container">
+          <div class="row">
+            <div class="col-3">
+              <img src="src/components/footerImgs/icon.png" alt="titleicon.png" width="300px" />
+            </div>
+            <div class="col-6">
+              <div class="row">
+                &nbsp;
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  <a href=""> <img src="src/components/footerImgs/Facebook.png" alt="facebook.png" /> </a>
+                  <a href=""><img src="src/components/footerImgs/Instagram.png" alt="instagram.png" /> </a>
+                  <a href=""> <img src="src/components/footerImgs/Youtube.png" alt="youtube.png" /> </a>
+                  <a href=""> <img src="src/components/footerImgs/Twitter.png" alt="twitter.png" /> </a>
+                </div>
+                <div class="row">
+                  &nbsp;
+                </div>
+              </div>
+            </div>
+
+            <div class="col-2">
+              <div class="row">
+                &nbsp;
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  Av. de la Arruzafilla, s/n,
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  14011 Córdoba
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  957 37 99 45
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12">
+
+                </div>
+              </div>
+
+            </div>
+
+          </div>
         </div>
-
       </footer>
-      <div className='bottom'>&nbsp;</div>
+      <div className='bottom'>
+        <div class="row">
+
+          <div class="col-9">
+            &nbsp;
+          </div>
+          <div class="col-3">
+
+          </div>
+        </div>
+      </div>
     </div>
 
   )
