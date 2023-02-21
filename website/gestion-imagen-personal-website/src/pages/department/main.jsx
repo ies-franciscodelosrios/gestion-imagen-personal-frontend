120234aa2badb50b5801e6ed4fa75bdbaf31ef58
Jose_Angel_Marin_Moreno <joseangelmarin1234@gmail.com>
2023-02-21 14:09:09 +0100

    departament 1.0

--- website/gestion-imagen-personal-website/src/pages/department/main.jsx
+++ website/gestion-imagen-personal-website/src/pages/department/main.jsx
@@ -1,11 +1,71 @@
-import {  } from 'react'
+import { } from 'react'
 import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function main() {
 
   return (
-    <div className="main">
-     
+    <div className="Departament">
+
+
+
+      <h1 className='title'>Departamentos</h1>
+
+      <div class="body">
+        <div draggable="false" class="container pt-5" data-v-271253ee="">
+          <div class="mb-10 text-center text-lg-start">
+            <div class='row gx-lg-5'>
+              <div class='col-lg-3 col-md-12 mb-4 mb-lg-0 '>
+
+                <div class="card border border-primary border-2">
+                  <h5>Departamento de Peluquería y cosmética capilar</h5>
+                  <p>
+                    La competencia general de este título consiste en realizar el cuidado y embellecimiento del cabello, la estética de manos y pies y el estilismo masculino, así como comercializar servicios y venta de cosméticos, cumpliendo los protocolos de calidad, prevención de riesgos laborales y protección ambiental
+                  </p>
+                </div>
+              </div>
+
+              <div class='col-lg-3 col-md-12 mb-4 mb-lg-0 '>
+                <div class="card border border-primary border-2">
+                  <h5>Departamento de Estilismo y dirección de peluquería</h5>
+                  <p>
+                    La competencia general de este título consiste en realizar el cuidado y embellecimiento del cabello, la estética de manos y pies y el estilismo masculino, así como comercializar servicios y venta de cosméticos, cumpliendo los protocolos de calidad, prevención de riesgos laborales y protección ambiental
+                  </p>
+                </div>
+              </div>
+
+
+
+              <div class='col-lg-3 col-md-12 mb-4 mb-lg-0 '>
+                <div class="card border border-primary border-2">
+                  <h5>
+                  Departamento de Estética integral y bienestar
+                  </h5>
+                  <p>
+                    La competencia general de este título consiste en dirigir el de desarrollo de servicios esteticos, planificar y realizar tratamientos esteticos y aplicar técnicas estéticas hidrotermales, depilación e implantación de pigmentos, asegurando la calidad y preveyendo riesgos laborales y gestión ambiental.
+                  </p>
+                </div>
+              </div>
+
+
+
+              <div class='col-lg-3 col-md-12 mb-4 mb-lg-0 '>
+                <div class="card border border-primary border-2">
+                  <h5>
+                  Departamento de Estilismo y dirección de peluquería
+                  </h5>
+                  <p>
+                    La competencia general de este título consiste en dirigir y supervisar el desarrollo de los servicios de peluquería, planificar y realizar tratamientos capilares estéticos y diseñar estilismos de cambio de imagen, respetando criterios de calidad, seguridad, respecto al medio ambiente y diseño para todos.
+                  </p>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+        </div>  
+      </div>
+
     </div>
   )
 }
