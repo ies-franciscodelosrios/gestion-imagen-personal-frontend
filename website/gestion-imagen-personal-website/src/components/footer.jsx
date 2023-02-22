8eb02362fb53e182c93fd4460b8e3bf2fef27e5b
Jose_Angel_Marin_Moreno <joseangelmarin1234@gmail.com>
2023-02-22 12:34:49 +0100

    footer+departament

--- website/gestion-imagen-personal-website/src/components/footer.jsx
+++ website/gestion-imagen-personal-website/src/components/footer.jsx
@@ -11,13 +11,14 @@ function footer() {
   return (
     <footer className="padding_2x">
       <div className="flex">
+
         <section className="flex-content padding_1x">
           <h3>Horario</h3>
           <table>
             <tr>
               <th>
                 Mañana
-              </th> 
+              </th>
               <th>
                 Tarde
               </th>
@@ -26,15 +27,15 @@ function footer() {
               <th>
                 8:15-14:30
               </th>
-               <th>
+              <th>
                 15:30-21:30
               </th>
             </tr>
-           
+
           </table>
-         
+
         </section>
-       
+
         <section className="flex-content padding_1x">
           <h3>Direccion</h3>
           <p> Av. de la Arruzafilla, s/n</p>
@@ -45,14 +46,13 @@ function footer() {
         <section className="flex-content padding_1x">
           <h3>Redes sociales</h3>
           <fieldset className="fixed_flex">
-          <a href="#"><img src="src/components/footerImgs/facebook.png" alt="facebook" width="50px" /></a>
-          <a href="https://mobile.twitter.com/iestablero"><img src="src/components/footerImgs/twitter.png" alt="twitter" width="50px" /></a>
-          <a href="#"><img src="src/components/footerImgs/gmail.png" alt="gmail" width="50px" /></a>
-          <a href="https://es.linkedin.com/in/isabel-rodriguez-morales-6372b344"><img src="src/components/footerImgs/linkedin.png" alt="linkedin" width="50px" /></a>
-   </fieldset>
+            <a href="https://www.facebook.com/people/IES-El-Tablero/100063569645527/"><img src="src/components/footerImgs/facebook.png" alt="facebook" width="50px" /></a>
+            <a href="https://mobile.twitter.com/iestablero"><img src="src/components/footerImgs/twitter.png" alt="twitter" width="50px" /></a>
+            <a href="mailto:"><img src="src/components/footerImgs/gmail.png" alt="gmail" width="50px" /></a>
+            <a href="https://es.linkedin.com/in/isabel-rodriguez-morales-6372b344"><img src="src/components/footerImgs/linkedin.png" alt="linkedin" width="50px" /></a>
+          </fieldset>
         </section>
       </div>
-      <div className="flex"></div>{/* no borrar, se modifica lo de arriba */}
     </footer>
   )
 }
--- website/gestion-imagen-personal-website/src/pages/department/main.jsx
+++ website/gestion-imagen-personal-website/src/pages/department/main.jsx
@@ -1,11 +1,210 @@
-import {  } from 'react'
+import { } from 'react'
 import './style.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function main() {
 
   return (
-    <div className="main">
-     
+    <div className="Departament">
+
+
+
+
+      <div className='title unselectable'>
+        <h1 >
+          <img src="src/pages/department/img/icon.png" alt="icono" width="5%" />
+          Departamento
+        </h1>
+      </div>
+      <div className="underTitle unselectable">
+        <h2 >Cursos</h2>
+      </div>
+      <div className="body">
+
+
+        <div draggable="false" className="container pt-5" data-v-271253ee="">
+          <div className="mb-10 text-center text-lg-start">
+            <div className='row gx-lg-5'>
+              <div className='col-lg-3 col-md-12 mb-4 mb-lg-0 '>
+
+                <div className="card ">
+                  <h5>Departamento de Peluquería y cosmética capilar</h5>
+                  <p>
+                    La competencia general de este título consiste en realizar el cuidado y embellecimiento del cabello, la estética de manos y pies y el estilismo masculino, así como comercializar servicios y venta de cosméticos, cumpliendo los protocolos de calidad, prevención de riesgos laborales y protección ambiental
+                  </p>
+                </div>
+              </div>
+              <div className='col-lg-3 col-md-12 mb-4 mb-lg-0 '>
+                <div className="card ">
+                  <h5>Departamento de Estilismo y dirección de peluquería</h5>
+                  <p>
+                    La competencia general de este título consiste en realizar el cuidado y embellecimiento del cabello, la estética de manos y pies y el estilismo masculino, así como comercializar servicios y venta de cosméticos, cumpliendo los protocolos de calidad, prevención de riesgos laborales y protección ambiental
+                  </p>
+                </div>
+              </div>
+              <div className='col-lg-3 col-md-12 mb-4 mb-lg-0 '>
+                <div className="card ">
+                  <h5>
+                    Departamento de Estética integral y bienestar
+                  </h5>
+                  <p>
+                    La competencia general de este título consiste en dirigir el de desarrollo de servicios esteticos, planificar y realizar tratamientos esteticos y aplicar técnicas estéticas hidrotermales, depilación e implantación de pigmentos, asegurando la calidad y preveyendo riesgos laborales y gestión ambiental.
+                  </p>
+                </div>
+              </div>
+              <div className='col-lg-3 col-md-12 mb-4 mb-lg-0 '>
+                <div className="card">
+                  <h5>
+                    Departamento de Estilismo y dirección de peluquería
+                  </h5>
+                  <p>
+                    La competencia general de este título consiste en dirigir y supervisar el desarrollo de los servicios de peluquería, planificar y realizar tratamientos capilares estéticos y diseñar estilismos de cambio de imagen, respetando criterios de  seguridad hacia el medio ambiente y diseño para todos.
+                  </p>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+      <div className="underTitle">
+        <h2 >Profesorado</h2>
+      </div>
+      <div className="body2">
+
+
+        <div draggable="false" className="container pt-5" data-v-271253ee="">
+          <div className="mb-10 text-center text-lg-start">
+            <div className='row gx-lg-5'>
+            <div className='col-lg-2 col-md-12 mb-4 mb-lg-0 '>
+              </div >
+              <div className='col-lg-4 col-md-12 mb-4 mb-lg-0 '>
+
+                <div className="card ">
+                <div class='row'>
+                    <div class='col-2'>
+                      <img src="src/pages/department/img/perfil.png" alt="icon" width="64px" />
+                    </div>
+                    <div className='col-1'>
+                      &nbsp;
+                    </div>
+                    <div class='col-9'>
+                    <ul>
+                        <li>
+                          Nombre: Manuel Moralez
+                        </li>
+                        <li>
+                          Asignatura: Peluqueria
+                        </li>
+                        <li>
+                          Mail: example@gmail.es
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='col-lg-4 col-md-12 mb-4 mb-lg-0 '>
+                <div className="card ">
+                  <div class='row'>
+                    <div class='col-2'>
+                      <img src="src/pages/department/img/perfil.png" alt="icon" width="64px" />
+                    </div>
+                    <div className='col-1'>
+                      &nbsp;
+                    </div>
+                    <div class='col-9 '>
+                      <ul>
+                        <li>
+                          Nombre: Manuel Moralez
+                        </li>
+                        <li>
+                          Asignatura: Peluqueria
+                        </li>
+                        <li>
+                          Mail: example@gmail.es
+                        </li>
+                      </ul>
+                   
+                    </div>
+                  </div>
+
+
+                </div>
+              </div>
+              
+             
+
+            </div>
+            <div className='row gx-lg-5'>
+            <div className='col-lg-2 col-md-12 mb-4 mb-lg-0 '>
+              </div >
+              <div className='col-lg-4 col-md-12 mb-4 mb-lg-0 '>
+
+                <div className="card ">
+                <div class='row'>
+                    <div class='col-2'>
+                      <img src="src/pages/department/img/perfil.png" alt="icon" width="64px" />
+                    </div>
+                    <div className='col-1'>
+                      &nbsp;
+                    </div>
+                    <div class='col-9'>
+                    <ul>
+                        <li>
+                          Nombre: Manuel Moralez
+                        </li>
+                        <li>
+                          Asignatura: Peluqueria
+                        </li>
+                        <li>
+                          Mail: example@gmail.es
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='col-lg-4 col-md-12 mb-4 mb-lg-0 '>
+                <div className="card ">
+                  <div class='row'>
+                    <div class='col-2'>
+                      <img src="src/pages/department/img/perfil.png" alt="icon" width="64px" />
+                    </div>
+                    <div className='col-1'>
+                      &nbsp;
+                    </div>
+                    <div class='col-9 '>
+                      <ul>
+                        <li>
+                          Nombre: Manuel Moralez
+                        </li>
+                        <li>
+                          Asignatura: Peluqueria
+                        </li>
+                        <li>
+                          Mail: example@gmail.es
+                        </li>
+                      </ul>
+                   
+                    </div>
+                  </div>
+
+
+                </div>
+              </div>
+              
+             
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+
+      </div>
+
     </div>
   )
 }
