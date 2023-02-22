a4f2ebc16f649df8e81cfd75fd2e52fd9f1b7bac
JuanFrancisco21 <juanfranciscoaguilarsanchez@gmail.com>
2023-02-22 18:51:30 +0100

    Funcionalidades de redux

--- src/views/apps/cliente/store/index.js
+++ src/views/apps/cliente/store/index.js
@@ -3,14 +3,18 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 // ** Axios Imports
 import axios from 'axios'
+import { ApiDelClient, getAllClientsData } from '../../../../services/api'
 
 export const getAllData = createAsyncThunk('appUsers/getAllData', async () => {
-  const response = await axios.get('/api/users/list/all-data')
-  return response.data
+  const response = await getAllClientsData()
+  console.log(response);
+  
+  return response.data.users
 })
 
 export const getData = createAsyncThunk('appUsers/getData', async params => {
-  const response = await axios.get('/api/users/list/data', params)
+  const response = await getAllClientsData().then(result => {return result})
+  console.log(response);
   return {
     params,
     data: response.data.users,
@@ -31,7 +35,7 @@ export const addUser = createAsyncThunk('appUsers/addUser', async (user, { dispa
 })
 
 export const deleteUser = createAsyncThunk('appUsers/deleteUser', async (id, { dispatch, getState }) => {
-  await axios.delete('/apps/users/delete', { id })
+  await ApiDelClient(id)
   await dispatch(getData(getState().users.params))
   await dispatch(getAllData())
   return id
