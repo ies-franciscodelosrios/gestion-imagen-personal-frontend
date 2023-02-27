77acd0b505b0fccbed716c0f7b5141104fa53270
Jose_Angel_Marin_Moreno <joseangelmarin1234@gmail.com>
2023-02-27 17:25:12 +0100

    faqs

--- src/views/pages/faq/FaqContact.js
+++ src/views/pages/faq/FaqContact.js
@@ -1,12 +1,12 @@
 // ** Icons Imports
-import { PhoneCall, Mail } from 'react-feather'
+import { PhoneCall, Mail,MapPin } from 'react-feather'
 
 // ** Reactstrap Imports
 import { Row, Col, Card, CardBody } from 'reactstrap'
 
 const FaqContact = () => {
   return (
-    <div className='faq-contact'>
+    <div className='faq-contact' style={{ marginTop:"30px"}}>
       <Row className='mt-5 pt-75'>
         <Col className='text-center' sm='12'>
          {/*  <h2>¿Aun tienes preguntas?</h2>
@@ -14,7 +14,7 @@ const FaqContact = () => {
             Si no se ha resuelto con las soluciones del FAQ accede a la documentación de la misma.
           </p> */}
         </Col>
-        <Col sm='6'>
+        <Col sm='4'>
           <Card className='text-center faq-contact-card shadow-none py-1'>
             <CardBody>
               <div className='avatar avatar-tag bg-light-primary mb-2 mx-auto'>
@@ -25,14 +25,26 @@ const FaqContact = () => {
             </CardBody>
           </Card>
         </Col>
-        <Col sm='6'>
+      
+        <Col sm='4'>
           <Card className='text-center faq-contact-card shadow-none py-1'>
             <CardBody>
               <div className='avatar avatar-tag bg-light-primary mb-2 mx-auto'>
-                <Mail size={18} />
+              <a href="mailTo:iestablero@help.com"><Mail size={18} /></a>
               </div>
               <h4>iestablero@help.com</h4>
               <span className='text-body'>La mejor manera de conseguir una rápida respuesta!</span>
+            </CardBody>
+          </Card>
+        </Col> 
+         <Col sm='4'>
+          <Card className='text-center faq-contact-card shadow-none py-1'>
+            <CardBody>
+              <div className='avatar avatar-tag bg-light-primary mb-2 mx-auto'>
+              <a href="https://goo.gl/maps/6kwjE5LRAabU4PGm7">  <MapPin size={18} /></a>
+              </div>
+              <h4>Av. de la Arruzafilla, s/n, 14011 Córdoba</h4>
+              <span className='text-body'>Visitanos cuando quieras!</span>
             </CardBody>
           </Card>
         </Col>
--- src/views/pages/faq/Faqs.js
+++ src/views/pages/faq/Faqs.js
@@ -54,8 +54,8 @@ const Faqs = ({ data }) => {
       const IconTag = Icon[item.icon]
 
       return (
-        <TabPane key={item.title} tabId={item.title}>
-          <div className='d-flex align-items-center'>
+        <TabPane key={item.title} tabId={item.title} >
+          <div className='d-flex align-items-center'  >
             <div className='avatar avatar-tag bg-light-primary me-1'>
               <IconTag size={20} />
             </div>
