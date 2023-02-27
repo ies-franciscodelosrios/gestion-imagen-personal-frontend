4f2b8c334de4b6873760b1600d56973bf3298ece
Jose_Angel_Marin_Moreno <joseangelmarin1234@gmail.com>
2023-02-27 17:25:12 +0100

    faqs

--- src/views/pages/faq/FaqFilter.js
+++ src/views/pages/faq/FaqFilter.js
@@ -1,9 +1,9 @@
 // ** Icons Imports
 import { Search } from 'react-feather'
-
 // ** Reactstrap Imports
 import { Card, CardBody, CardText, Form, Input, InputGroup, InputGroupText } from 'reactstrap'
-
+// ** Images
+import logo from '@src/assets/images/logo/logo.svg'
 const FaqFilter = ({ searchTerm, setSearchTerm, getFAQData }) => {
   const handleFaqFilter = e => {
     setSearchTerm(e.target.value)
@@ -15,12 +15,30 @@ const FaqFilter = ({ searchTerm, setSearchTerm, getFAQData }) => {
       <Card
         className='faq-search'
         style={{
+          margin: "auto",
+          width: "900px",
+          height: "350px",
+          border: "5px solid #e6f1f1",
+          "border-radius": "20px",
+          "margin-bottom":"20px",
+          "border-style": "outset",
+
+
           backgroundImage: `url('@src/assets/images/banner/banner.png')`
         }}
       >
         <CardBody className='text-center'>
+          <img
+            alt='logo'
+            src={logo}
+            style={{ width: "100px", margin: "auto" }}
+            className='img-fluid d-none d-md-block'
+          />
           <h2 className='text-primary'>¿Tienes Alguna pregunta?</h2>
           <CardText className='mb-2'>Busca entre nuestras preguntas más comunes o preguntanos directamente </CardText>
+
+
+
           {/* <Form className='faq-search-input' onSubmit={e => e.preventDefault()}>
             <InputGroup className='input-group-merge'>
               <InputGroupText>
