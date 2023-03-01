7442c56121b848a7263fe6acf12943f91f712219
zalobmq <gbretonesmoraquero@gmail.com>
2023-03-01 10:20:30 +0100

    files store and services api

--- src/views/apps/profesor/store/index.js
+++ src/views/apps/profesor/store/index.js
@@ -3,14 +3,19 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 // ** Axios Imports
 import axios from 'axios'
+import { getAllProfesorData, getUserById ,DelUser } from '../../../../services/api'
+
+/* ALL PROFESOR */
 
 export const getAllData = createAsyncThunk('appUsers/getAllData', async () => {
-  const response = await axios.get('/api/users/list/all-data')
-  return response.data
+  const response = await getAllProfesorData().then(result => {return result}) 
+  return response.data.users
 })
 
+/*  */
+
 export const getData = createAsyncThunk('appUsers/getData', async params => {
-  const response = await axios.get('/api/users/list/data', params)
+  const response = await getAllProfesorData().then(result => {return result})
   return {
     params,
     data: response.data.users,
@@ -18,8 +23,11 @@ export const getData = createAsyncThunk('appUsers/getData', async params => {
   }
 })
 
+/* GET USER BY ID */
+
 export const getUser = createAsyncThunk('appUsers/getUser', async id => {
-  const response = await axios.get('/api/users/user', { id })
+  const response = await getUserById(id).then(result => {return result})
+  console.log(response)
   return response.data.user
 })
 
@@ -30,8 +38,10 @@ export const addUser = createAsyncThunk('appUsers/addUser', async (user, { dispa
   return user
 })
 
+/* DELETE USER BY ID */
+
 export const deleteUser = createAsyncThunk('appUsers/deleteUser', async (id, { dispatch, getState }) => {
-  await axios.delete('/apps/users/delete', { id })
+  await DelUser(id)
   await dispatch(getData(getState().users.params))
   await dispatch(getAllData())
   return id
