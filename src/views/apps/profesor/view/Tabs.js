c68e28a77b73f79fc57c7c507e4d5803641dbdfc
Jose_Angel_Marin_Moreno <joseangelmarin1234@gmail.com>
2023-05-13 13:35:25 +0200

    modificaciones

--- src/views/apps/profesor/view/Tabs.js
+++ src/views/apps/profesor/view/Tabs.js
@@ -2,7 +2,7 @@
 import { Fragment } from 'react'
 
 // ** Reactstrap Imports
-import { Nav, NavItem, TabContent, TabPane } from 'reactstrap'
+import { Nav, NavItem, TabContent, TabPane,NavLink } from 'reactstrap'
 
 // ** Icons Imports
 import { Book } from 'react-feather'
@@ -11,27 +11,57 @@ import { Book } from 'react-feather'
 
 
 import Tratamientos from './Tratamientos'
+import Alumnos from './Alumnos'
+
+
+const toggle = (tab) => {
+  if (active !== tab) {
+    setActive(tab);
+  }
+  handleReset();
+};
 
 const UserTabs = ({ active, toggleTab }) => {
   return (
     <Fragment>
-      <Nav pills className='mb-2'>
-        
-       
+      <Nav tabs className='mb-2'> 
+
         <NavItem>
-       
+          <NavLink
+            active={active === '1'}
+            onClick={() => {
+              toggle('1');
+            }}
+          >
             <Book className='font-medium-3 me-50' />
-            <span className='fw-bold'>Tratamientos Activos</span>
-        
+            <span className='fw-bold'>Alumnos</span>
+          </NavLink>
+
         </NavItem>
-       
+
+
+        <NavItem>
+          <NavLink
+            active={active === '2'}
+            onClick={() => {
+              toggle('2');
+            }}
+          >
+            <Book className='font-medium-3 me-50' />
+            <span className='fw-bold'>Tratamientos</span>
+          </NavLink>
+
+        </NavItem>
       </Nav>
       <TabContent activeTab={active}>
-       
-       
-        <TabPane tabId='1'>
+
+      <TabPane tabId='1'>
+          <Alumnos />
+        </TabPane>
+        <TabPane tabId='2'>
           <Tratamientos />
         </TabPane>
+      
       </TabContent>
     </Fragment>
   )
