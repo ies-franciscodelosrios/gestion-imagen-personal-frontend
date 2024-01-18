4f6492341b63a52201b3c7a72b2322167b15a58e
Juanmagw <juanma4x21@gmail.com>
2024-01-18 08:43:33 +0100

    Adding eslint options to typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
   parser: '@babel/eslint-parser',
   env: {
     node: true,
