6af0ea39c83d45cec8263f2957b54153bbfea78c
RafaTicArte <rafa@ticarte.com>
2024-01-26 23:31:16 +0100

    Rename user->student folder

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,12 +1,12 @@
 // ** Reducers Imports
-import navbar from './navbar'
-import layout from './layout'
-import auth from './authentication'
-import clients from '@src/views/apps/client/store'
-import users from '@src/views/apps/user/store'
-import profesor from '@src/views/apps/profesor/store'
-import calendar from '@src/views/apps/calendar/store'
-import ecommerce from '@src/views/apps/ecommerce/store'
+import navbar from "./navbar";
+import layout from "./layout";
+import auth from "./authentication";
+import clients from "@src/views/apps/client/store";
+import users from "@src/views/apps/student/store";
+import profesor from "@src/views/apps/profesor/store";
+import calendar from "@src/views/apps/calendar/store";
+import ecommerce from "@src/views/apps/ecommerce/store";
 
 const rootReducer = {
   auth,
@@ -17,6 +17,6 @@ const rootReducer = {
   layout,
   calendar,
   ecommerce,
-}
+};
 
-export default rootReducer
+export default rootReducer;
--- src/views/apps/profile/UserInfoCard.js
+++ src/views/apps/profile/UserInfoCard.js
@@ -1,7 +1,7 @@
 // ** React Imports
-import { useState, Fragment } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { updateUser } from '../user/store';
+import { useState, Fragment } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateUser } from "../student/store";
 
 // ** Reactstrap Imports
 import {
@@ -16,44 +16,54 @@ import {
   Input,
   Label,
   ModalBody,
-  ModalHeader
-} from 'reactstrap'
+  ModalHeader,
+} from "reactstrap";
 
 // ** Third Party Components
-import Swal from 'sweetalert2'
-import Select from 'react-select'
-import { Check, Briefcase, X } from 'react-feather'
-import { useForm, Controller } from 'react-hook-form'
-import withReactContent from 'sweetalert2-react-content'
+import Swal from "sweetalert2";
+import Select from "react-select";
+import { Check, Briefcase, X } from "react-feather";
+import { useForm, Controller } from "react-hook-form";
+import withReactContent from "sweetalert2-react-content";
 
 // ** Custom Components
-import Avatar from '@components/avatar'
+import Avatar from "@components/avatar";
 
 // ** Utils
-import { selectThemeColors } from '@utils'
+import { selectThemeColors } from "@utils";
 
 // ** Styles
-import '@styles/react/libs/react-select/_react-select.scss'
-import { toast } from 'react-hot-toast';
-import { validateDNI, validateUserData } from '../../../utility/Utils';
+import "@styles/react/libs/react-select/_react-select.scss";
+import { toast } from "react-hot-toast";
+import { validateDNI, validateUserData } from "../../../utility/Utils";
 
 const cycleOptions = [
-  { label: 'Grado Medio - Peluquería y cosmética capilar', value: 'Grado Medio - Peluquería y cosmética capilar' },
-  { label: 'Grado Medio - Estética y belleza', value: 'Grado Medio - Estética y belleza' },
-  { label: 'Grado Superior - Estética integral y bienestar', value: 'Grado Superior - Estética integral y bienestar' },
-  { label: 'Grado Superior - Estilismo y dirección de peluquería', value: 'Grado Superior - Estilismo y dirección de peluquería' },
+  {
+    label: "Grado Medio - Peluquería y cosmética capilar",
+    value: "Grado Medio - Peluquería y cosmética capilar",
+  },
+  {
+    label: "Grado Medio - Estética y belleza",
+    value: "Grado Medio - Estética y belleza",
+  },
+  {
+    label: "Grado Superior - Estética integral y bienestar",
+    value: "Grado Superior - Estética integral y bienestar",
+  },
+  {
+    label: "Grado Superior - Estilismo y dirección de peluquería",
+    value: "Grado Superior - Estilismo y dirección de peluquería",
+  },
 ];
-
 
 const UserInfoCard = () => {
   // ** Store Vars
   const dispatch = useDispatch();
-  const store = useSelector(state => state.users)
-
+  const store = useSelector((state) => state.users);
 
   // ** State
   const selectedUser = store.selectedUser;
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
 
   // ** Hook
   const {
@@ -61,41 +71,41 @@ const UserInfoCard = () => {
     control,
     setError,
     handleSubmit,
-    formState: { errors }
+    formState: { errors },
   } = useForm({
     defaultValues: {
-      name: '',
-      surname: '',
-      email: '',
-      dni: '',
-      cycle: '',
-      course_year: '',
-      password: '',
-      repassword: '',
-    }
-  })
+      name: "",
+      surname: "",
+      email: "",
+      dni: "",
+      cycle: "",
+      course_year: "",
+      password: "",
+      repassword: "",
+    },
+  });
 
   // ** render user img
   const renderUserImg = () => {
     return (
       <Avatar
         initials
-        color={'light-primary'}
-        className='rounded mt-3 mb-2'
+        color={"light-primary"}
+        className="rounded mt-3 mb-2"
         content={selectedUser.name}
         contentStyles={{
           borderRadius: 0,
-          fontSize: 'calc(48px)',
-          width: '100%',
-          height: '100%'
+          fontSize: "calc(48px)",
+          width: "100%",
+          height: "100%",
         }}
         style={{
-          height: '110px',
-          width: '110px'
+          height: "110px",
+          width: "110px",
         }}
       />
-    )
-  }
+    );
+  };
 
   const onSubmit = (data) => {
     const updatedUser = { ...store.selectedUser };
@@ -107,21 +117,21 @@ const UserInfoCard = () => {
     updatedUser.course_year = data.course_year;
     updatedUser.password = data.password;
     updatedUser.repassword = data.repassword;
-    
+
     if (validateUserData(data)) {
       dispatch(updateUser(updatedUser));
       setShow(false);
     } else {
       for (const key in data) {
-        if (!validateDNI(data.dni))setError('dni',{})
-        if (data[key].length === 0 && !key.includes('pass')) {
+        if (!validateDNI(data.dni)) setError("dni", {});
+        if (data[key].length === 0 && !key.includes("pass")) {
           setError(key, {
-            type: 'manual'
-          })
+            type: "manual",
+          });
         }
       }
     }
-  }
+  };
 
   const handleReset = () => {
     reset({
@@ -130,11 +140,11 @@ const UserInfoCard = () => {
       email: selectedUser.email,
       dni: selectedUser.dni,
       cycle: selectedUser.cycle.label,
-      course_year:selectedUser.course_year,
-      password: '',
-      repassword: '',
-    })
-  }
+      course_year: selectedUser.course_year,
+      password: "",
+      repassword: "",
+    });
+  };
 
   return (
     <Fragment>
@@ -147,10 +157,9 @@ const UserInfoCard = () => {
                 <div className="user-info mb-3">
                   <h4>
                     {selectedUser !== null
-                      ? selectedUser.name.concat(' ' + selectedUser.surname)
-                      : 'Eleanor Aguilar'}
+                      ? selectedUser.name.concat(" " + selectedUser.surname)
+                      : "Eleanor Aguilar"}
                   </h4>
-
                 </div>
               </div>
             </div>
@@ -184,7 +193,13 @@ const UserInfoCard = () => {
             ) : null}
           </div>
           <div className="d-flex justify-content-center pt-2">
-            <Button color="primary" onClick={() => { handleReset(); setShow(true) }}>
+            <Button
+              color="primary"
+              onClick={() => {
+                handleReset();
+                setShow(true);
+              }}
+            >
               Editar
             </Button>
           </div>
@@ -193,59 +208,59 @@ const UserInfoCard = () => {
       <Modal
         isOpen={show}
         toggle={() => setShow(!show)}
-        className='modal-dialog-centered modal-lg'
+        className="modal-dialog-centered modal-lg"
       >
         <ModalHeader
-          className='bg-transparent'
+          className="bg-transparent"
           toggle={() => setShow(!show)}
         ></ModalHeader>
-        <ModalBody className='px-sm-5 pt-50 pb-5'>
-          <div className='text-center mb-2'>
-            <h1 className='mb-1'>Editar Información</h1>
+        <ModalBody className="px-sm-5 pt-50 pb-5">
+          <div className="text-center mb-2">
+            <h1 className="mb-1">Editar Información</h1>
             <p>Actualiza los datos del estudiante de manera segura.</p>
           </div>
           <Form onSubmit={handleSubmit(onSubmit)}>
-            <Row className='gy-1 pt-75'>
+            <Row className="gy-1 pt-75">
               <Col md={6} xs={12}>
-                <Label className='form-label' for='name'>
+                <Label className="form-label" for="name">
                   Nombre
                 </Label>
                 <Controller
                   defaultValue={selectedUser.name}
                   control={control}
-                  id='name'
-                  name='name'
+                  id="name"
+                  name="name"
                   render={({ field }) => (
                     <Input
                       {...field}
-                      id='name'
-                      placeholder='Laura'
+                      id="name"
+                      placeholder="Laura"
                       invalid={errors.name && true}
                     />
                   )}
                 />
               </Col>
               <Col md={6} xs={12}>
-                <Label className='form-label' for='surname'>
+                <Label className="form-label" for="surname">
                   Apellidos
                 </Label>
                 <Controller
                   defaultValue={selectedUser.surname}
                   control={control}
-                  id='surname'
-                  name='surname'
+                  id="surname"
+                  name="surname"
                   render={({ field }) => (
                     <Input
                       {...field}
-                      id='surname'
-                      placeholder='Torres'
+                      id="surname"
+                      placeholder="Torres"
                       invalid={errors.surname && true}
                     />
                   )}
                 />
               </Col>
               <Col xs={12}>
-                <Label className='form-label' for='email'>
+                <Label className="form-label" for="email">
                   Email
                 </Label>
                 <Controller
@@ -274,7 +289,12 @@ const UserInfoCard = () => {
                   id="dni"
                   name="dni"
                   render={({ field }) => (
-                    <Input {...field} id="dni" placeholder="31000000C" invalid={errors.dni && true}/>
+                    <Input
+                      {...field}
+                      id="dni"
+                      placeholder="31000000C"
+                      invalid={errors.dni && true}
+                    />
                   )}
                 />
               </Col>
@@ -283,7 +303,10 @@ const UserInfoCard = () => {
                   Ciclo <span className="text-danger">*</span>
                 </Label>
                 <Controller
-                  defaultValue={{label:selectedUser.cycle, value:selectedUser.cycle}} // Set the default value to the first option in the array
+                  defaultValue={{
+                    label: selectedUser.cycle,
+                    value: selectedUser.cycle,
+                  }} // Set the default value to the first option in the array
                   control={control}
                   id="cycle"
                   name="cycle"
@@ -292,10 +315,10 @@ const UserInfoCard = () => {
                       {...field}
                       options={cycleOptions}
                       theme={selectThemeColors}
-                      className='react-select'
-                      classNamePrefix='select'
+                      className="react-select"
+                      classNamePrefix="select"
                       id="cycle"
-                      name='cycle'
+                      name="cycle"
                       placeholder="Elige tu ciclo"
                       invalid={errors.cycle && true}
                     />
@@ -303,53 +326,53 @@ const UserInfoCard = () => {
                 />
               </Col>
               <Col md={6} xs={12}>
-                <Label className='form-label' for='password'>
+                <Label className="form-label" for="password">
                   Contraseña
                 </Label>
                 <Controller
                   control={control}
-                  id='password'
-                  name='password'
+                  id="password"
+                  name="password"
                   render={({ field }) => (
                     <Input
                       {...field}
-                      id='password'
-                      placeholder='Contraseña...'
+                      id="password"
+                      placeholder="Contraseña..."
                       invalid={errors.password && true}
                     />
                   )}
                 />
-              </Col>              
+              </Col>
               <Col md={6} xs={12}>
-                <Label className='form-label' for='repassword'>
+                <Label className="form-label" for="repassword">
                   Repite Contraseña
                 </Label>
                 <Controller
                   control={control}
-                  id='repassword'
-                  name='repassword'
+                  id="repassword"
+                  name="repassword"
                   render={({ field }) => (
                     <Input
                       {...field}
-                      id='repassword'
-                      placeholder='Repite Contraseña...'
+                      id="repassword"
+                      placeholder="Repite Contraseña..."
                       invalid={errors.repassword && true}
                     />
                   )}
                 />
               </Col>
-              <Col xs={12} className='text-center mt-2 pt-50'>
+              <Col xs={12} className="text-center mt-2 pt-50">
                 <Button type="submit" className="me-1" color="primary">
                   Guardar
                 </Button>
                 <Button
-                  type='reset'
-                  color='secondary'
+                  type="reset"
+                  color="secondary"
                   outline
                   onClick={() => {
-                    handleReset()
-                    setShow(false)
-                    toast.error('Datos no guardados')
+                    handleReset();
+                    setShow(false);
+                    toast.error("Datos no guardados");
                   }}
                 >
                   Cancelar
@@ -361,7 +384,6 @@ const UserInfoCard = () => {
       </Modal>
     </Fragment>
   );
+};
 
-}
-
-export default UserInfoCard
+export default UserInfoCard;
